9e7125f56ac0795e2401e842cf40d8c824cdde98
권정훈 <righthun.kwon@gmail.com>
2024-03-16 23:08:01 +0900

    react router

--- 한입리액트/section12/chapter03/src/App.jsx
+++ 한입리액트/section12/chapter03/src/App.jsx
@@ -1,9 +1,14 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Diary from "./pages/Diary";
-import New from "./pages/New";
-import Notfound from "./pages/Notfound";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Notfound from './pages/Notfound';
+
+// React Router
+// npm i react-router-dom
+// package.json의 dependencies에 설치 확인 가능
+// main.jsx를 BrowserRouter로 감싸는 작업이 반드시 필요
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -13,10 +18,12 @@ function App() {
     <>
       <div>Hello</div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary' element={<Diary />} />
+
+        {/* 경로에 맞는 라우터가 없을 경우 */}
+        <Route path='*' element={<Notfound />} />
       </Routes>
     </>
   );
--- 한입리액트/section12/chapter03/src/main.jsx
+++ 한입리액트/section12/chapter03/src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // 라우터 설정
   <BrowserRouter>
     <App />
   </BrowserRouter>
