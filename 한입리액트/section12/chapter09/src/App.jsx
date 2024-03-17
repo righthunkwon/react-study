92336955b4a6f03939f7d467c2ce6778fa825b64
권정훈 <righthun.kwon@gmail.com>
2024-03-17 12:57:02 +0900

    crud & useContext

--- 한입리액트/section12/chapter09/src/App.jsx
+++ 한입리액트/section12/chapter09/src/App.jsx
@@ -1,48 +1,57 @@
-import "./App.css";
-import { useReducer, useRef, createContext } from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Diary from "./pages/Diary";
-import New from "./pages/New";
-import Edit from "./pages/Edit";
-import Notfound from "./pages/Notfound";
+import './App.css';
+import { useReducer, useRef, createContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Notfound from './pages/Notfound';
 
 const mockData = [
   {
     id: 1,
     createdDate: new Date().getTime(),
     emotionId: 1,
-    content: "1번 일기 내용",
+    content: '1번 일기 내용',
   },
   {
     id: 2,
     createdDate: new Date().getTime(),
     emotionId: 2,
-    content: "2번 일기 내용",
+    content: '2번 일기 내용',
   },
 ];
 
 function reducer(state, action) {
   switch (action.type) {
-    case "CREATE":
+    // 일기 추가
+    // 스프레드 연산자(...) 활용
+    case 'CREATE':
       return [action.data, ...state];
-    case "UPDATE":
+
+    // 일기 수정
+    // map() 메서드 활용
+    // 배열 내의 모든 요소 각각에 대해 주어진 함수를 호출한 결과를 모아 새로운 배열 반환
+    // 각 요소마다의 id와 action 객체의 id가 같은지 검사하여 같으면 action.data를, 다르면 기존 요소를 반환
+    case 'UPDATE':
       return state.map((item) =>
-        String(item.id) === String(action.data.id)
-          ? action.data
-          : item
+        // 타입이 다른 오류를 방지하기 위해 문자열로 변환 후 검사
+        String(item.id) === String(action.data.id) ? action.data : item
       );
-    case "DELETE":
-      return state.filter(
-        (item) => String(item.id) !== String(action.id)
-      );
+
+    // 일기 삭제
+    // filter() 메서드 활용
+    // 주어진 배열의 얕은 복사본을 생성하고, 주어진 함수에 의해 구현된 테스트를 통과한(결과가 true) 요소만 필터링
+    case 'DELETE':
+      // 타입이 다른 오류를 방지하기 위해 문자열로 변환 후 검사
+      return state.filter((item) => String(item.id) !== String(action.id));
     default:
       return state;
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+const DiaryStateContext = createContext(); // 일기 데이터 공급
+const DiaryDispatchContext = createContext(); // 일기 데이터 수정하는 함수 공급
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
@@ -51,7 +60,7 @@ function App() {
   // 새로운 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
     dispatch({
-      type: "CREATE",
+      type: 'CREATE',
       data: {
         id: idRef.current++,
         createdDate,
@@ -64,7 +73,7 @@ function App() {
   // 기존 일기 수정
   const onUpdate = (id, createdDate, emotionId, content) => {
     dispatch({
-      type: "UPDATE",
+      type: 'UPDATE',
       data: {
         id,
         createdDate,
@@ -77,7 +86,7 @@ function App() {
   // 기존 일기 삭제
   const onDelete = (id) => {
     dispatch({
-      type: "DELETE",
+      type: 'DELETE',
       id,
     });
   };
@@ -86,18 +95,18 @@ function App() {
     <>
       <button
         onClick={() => {
-          onCreate(new Date().getTime(), 1, "Hello");
+          onCreate(new Date().getTime(), 1, 'Hello');
         }}
       >
-        일기 추가 테스트
+        1번 일기 추가 테스트
       </button>
 
       <button
         onClick={() => {
-          onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다");
+          onUpdate(1, new Date().getTime(), 3, '수정된 일기입니다');
         }}
       >
-        일기 수정 테스트
+        1번 일기 수정 테스트
       </button>
 
       <button
@@ -105,7 +114,7 @@ function App() {
           onDelete(1);
         }}
       >
-        일기 삭제 테스트
+        1번 일기 삭제 테스트
       </button>
 
       <DiaryStateContext.Provider value={data}>
@@ -117,11 +126,11 @@ function App() {
           }}
         >
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new" element={<New />} />
-            <Route path="/diary/:id" element={<Diary />} />
-            <Route path="/edit/:id" element={<Edit />} />
-            <Route path="*" element={<Notfound />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/new' element={<New />} />
+            <Route path='/diary/:id' element={<Diary />} />
+            <Route path='/edit/:id' element={<Edit />} />
+            <Route path='*' element={<Notfound />} />
           </Routes>
         </DiaryDispatchContext.Provider>
       </DiaryStateContext.Provider>
