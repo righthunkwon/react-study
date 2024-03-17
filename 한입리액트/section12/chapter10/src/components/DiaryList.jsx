4a30c865f8677f831beec47d3f3e1e59e7724a72
권정훈 <righthun.kwon@gmail.com>
2024-03-17 13:38:41 +0900

    home ui

--- 한입리액트/section12/chapter10/src/components/DiaryList.jsx
+++ 한입리액트/section12/chapter10/src/components/DiaryList.jsx
@@ -1,18 +1,20 @@
-import "./DiaryList.css";
-import Button from "./Button";
-import DiaryItem from "./DiaryItem";
+import './DiaryList.css';
+import Button from './Button';
+import DiaryItem from './DiaryItem';
 
 const DiaryList = () => {
   return (
-    <div className="DiaryList">
-      <div className="menu_bar">
+    <div className='DiaryList'>
+      {/* menu_bar */}
+      <div className='menu_bar'>
         <select>
-          <option value={"latest"}>최신순</option>
-          <option value={"oldest"}>오래된 순</option>
+          <option value={'latest'}>최신순</option>
+          <option value={'oldest'}>오래된 순</option>
         </select>
-        <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
+        <Button text={'새 일기 쓰기'} type={'POSITIVE'} />
       </div>
-      <div className="list_wrapper">
+      {/* List wrapper */}
+      <div className='list_wrapper'>
         <DiaryItem />
       </div>
     </div>
