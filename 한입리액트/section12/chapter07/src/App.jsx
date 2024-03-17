8d20425e9f9b01b8aa95a7b33ee46cdef732cf96
권정훈 <righthun.kwon@gmail.com>
2024-03-17 11:43:25 +0900

    common components

--- 한입리액트/section12/chapter07/src/App.jsx
+++ 한입리액트/section12/chapter07/src/App.jsx
@@ -1,65 +1,63 @@
-import "./App.css";
-import {
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Diary from "./pages/Diary";
-import New from "./pages/New";
-import Notfound from "./pages/Notfound";
-import Button from "./components/Button";
-import Header from "./components/Header";
+import './App.css';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Notfound from './pages/Notfound';
+import Button from './components/Button';
+import Header from './components/Header';
 
-import { getEmotionImage } from "./util/get-emotion-image";
+import { getEmotionImage } from './util/get-emotion-image';
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+// 프로젝트 개발 순서
+// 1. 페이지 라우팅
+// 2. 글로벌 레이아웃 설정
+// 3. 공통 컴포넌트 구현
+// 4. 개별 페이지 및 복잡한 기능 구현
+
 function App() {
   const nav = useNavigate();
 
   const onClickButton = () => {
-    nav("/new");
+    nav('/new');
   };
 
   return (
     <>
       <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
+        title={'Header'}
+        leftChild={<Button text={'Left'} />}
+        rightChild={<Button text={'Right'} />}
       />
 
       <Button
-        text={"123"}
+        text={'1번(default)'}
         onClick={() => {
-          console.log("123번 버튼 클릭!");
+          console.log('1번 버튼 클릭!');
         }}
       />
 
       <Button
-        text={"123"}
-        type={"POSITIVE"}
+        text={'2번(positive)'}
+        type={'POSITIVE'}
         onClick={() => {
-          console.log("123번 버튼 클릭!");
+          console.log('2번 버튼 클릭!');
         }}
       />
 
       <Button
-        text={"123"}
-        type={"NEGATIVE"}
+        text={'3번(negative'}
+        type={'NEGATIVE'}
         onClick={() => {
-          console.log("123번 버튼 클릭!");
+          console.log('3번 버튼 클릭!');
         }}
       />
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary/:id' element={<Diary />} />
+        <Route path='*' element={<Notfound />} />
       </Routes>
     </>
   );
--- 한입리액트/section12/chapter07/src/components/Button.jsx
+++ 한입리액트/section12/chapter07/src/components/Button.jsx
@@ -1,10 +1,10 @@
-import "./Button.css";
+import './Button.css';
 
 const Button = ({ text, type, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`Button Button_${type}`}
+      className={`Button Button_${type}`} // 동적 타입
     >
       {text}
     </button>
