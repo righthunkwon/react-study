8c150e68c80a1560513f61b3e17706b7da5cf8a4
권정훈 <righthun.kwon@gmail.com>
2024-03-31 16:39:20 +0900

    react study

--- 한입리액트/section12/chapter11/src/components/DiaryList.jsx
+++ 한입리액트/section12/chapter11/src/components/DiaryList.jsx
@@ -1,12 +1,12 @@
-import "./DiaryList.css";
-import Button from "./Button";
-import DiaryItem from "./DiaryItem";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import './DiaryList.css';
+import Button from './Button';
+import DiaryItem from './DiaryItem';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const DiaryList = ({ data }) => {
   const nav = useNavigate();
-  const [sortType, setSortType] = useState("latest");
+  const [sortType, setSortType] = useState('latest');
 
   const onChangeSortType = (e) => {
     setSortType(e.target.value);
@@ -14,7 +14,7 @@ const DiaryList = ({ data }) => {
 
   const getSortedData = () => {
     return data.toSorted((a, b) => {
-      if (sortType === "oldest") {
+      if (sortType === 'oldest') {
         return Number(a.createdDate) - Number(b.createdDate);
       } else {
         return Number(b.createdDate) - Number(a.createdDate);
@@ -25,19 +25,19 @@ const DiaryList = ({ data }) => {
   const sortedData = getSortedData();
 
   return (
-    <div className="DiaryList">
-      <div className="menu_bar">
+    <div className='DiaryList'>
+      <div className='menu_bar'>
         <select value={sortType} onChange={onChangeSortType}>
-          <option value={"latest"}>최신순</option>
-          <option value={"oldest"}>오래된 순</option>
+          <option value={'latest'}>최신순</option>
+          <option value={'oldest'}>오래된 순</option>
         </select>
         <Button
-          onClick={() => nav("/new")}
-          text={"새 일기 쓰기"}
-          type={"POSITIVE"}
+          onClick={() => nav('/new')}
+          text={'새 일기 쓰기'}
+          type={'POSITIVE'}
         />
       </div>
-      <div className="list_wrapper">
+      <div className='list_wrapper'>
         {sortedData.map((item) => (
           <DiaryItem key={item.id} {...item} />
         ))}
--- 한입리액트/section12/chapter11/src/pages/Home.jsx
+++ 한입리액트/section12/chapter11/src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import Header from "../components/Header";
-import Button from "../components/Button";
-import DiaryList from "../components/DiaryList";
-import { useState, useContext } from "react";
-import { DiaryStateContext } from "../App";
+import Header from '../components/Header';
+import Button from '../components/Button';
+import DiaryList from '../components/DiaryList';
+import { useState, useContext } from 'react';
+import { DiaryStateContext } from '../App';
 
+// 해당 월의 날짜를 검사하는 함수
 const getMonthlyData = (pivotDate, data) => {
   const beginTime = new Date(
     pivotDate.getFullYear(),
@@ -17,15 +18,15 @@ const getMonthlyData = (pivotDate, data) => {
   const endTime = new Date(
     pivotDate.getFullYear(),
     pivotDate.getMonth() + 1,
-    0,
+    0, // 0일로 설정할 경우 해당하는 달의 이전 달의 마지막 날로 세팅
     23,
     59,
     59
   ).getTime();
 
+  // 시작 일 이상 끝 일 이하인 날짜만 반환
   return data.filter(
-    (item) =>
-      beginTime <= item.createdDate && item.createdDate <= endTime
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
   );
 };
 
@@ -36,25 +37,20 @@ const Home = () => {
   const monthlyData = getMonthlyData(pivotDate, data);
 
   const onIncreaseMonth = () => {
-    setPivotDate(
-      new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1)
-    );
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
   };
 
   const onDecreaseMonth = () => {
-    setPivotDate(
-      new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1)
-    );
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
   };
 
   return (
     <div>
+      {/* 자바스크립트의 월은 0부터 시작되기 때문에 1을 더해서 사용해야 한다 */}
       <Header
-        title={`${pivotDate.getFullYear()}년 ${
-          pivotDate.getMonth() + 1
-        }월`}
-        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
-        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={'<'} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={'>'} />}
       />
       <DiaryList data={monthlyData} />
     </div>
